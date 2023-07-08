581408abe3ff5b1cb60f4f3edb8e6a841c425ffb
Thiago Weslley <thiagoweslley@yahoo.com>
2023-07-08 16:26:32 -0300

    feat: Adiciona biblioteca uuid para geração de keys

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-const App = () => {
-  const list = [{ id: 1, task: "Levar o Ted para passear"}, 
-  { id: 2, task: "Ler um livro sobre programação"}];
+import { v4 as uuidv4 } from "uuid";
 
+const App = () => {
+  const list = [
+    { id: uuidv4(), task: "Levar o Ted para passear" },
+    { id: uuidv4(), task: "Ler um livro sobre programação" },
+  ];
+  
   return (
     <div>
       <input placeholder="O que tenho para fazer..." />
