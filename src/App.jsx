5fa91966c3c9eff69f1d2126d84bc90b16316eb9
Thiago Weslley <thiagoweslley@yahoo.com>
2023-07-08 16:18:30 -0300

    feat: Estrutura HTML inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 const App = () => {
-  return;
-}
+  const list = [{ id: 1, task: "Levar o Ted para passear"}, 
+  { id: 2, task: "Ler um livro sobre programação"}];
+
+  return (
+    <div>
+      <input placeholder="O que tenho para fazer..." />
+      <button>Adicionar</button>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
